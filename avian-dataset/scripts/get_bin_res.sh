a10debcffa99260fb2e27a5641b381c2c4c5206a
Ashu Gupta <agupta@Ashus-MacBook-Pro.local>
2015-10-29 17:03:07 -0500

    Aggregate

--- avian-dataset/scripts/get_bin_res.sh
+++ avian-dataset/scripts/get_bin_res.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-for m in 0.5 1 2
+for t in 10 20 30 40
 do
-	for t in 10 20 30 40
+	for l in $(seq 1 100)
 	do
-		for l in $(seq 1 100)
-		do
-			python /home/agupta80/astral_wqmc/avian-dataset/src/get_bin.py "/home/agupta80/scratch/avian_dataset/avian-"$m"X-1000-500" 1 $l 100 $t
-			echo $m $t $l "Done"
-		done
+		python /home/agupta80/astral_wqmc/avian-dataset/src/get_bin.py /home/agupta80/scratch/avian_dataset/avian-0.5X-1000-500 1 $l 100 $t
+		echo $t $l "Done"
 	done
 done
---
+++ avian-dataset/src/quartet_aggregate_binning.py
@@ -0,0 +1,57 @@
+__author__ = 'ashu'
+import sys
+def formatted(i,j,k,l):
+    if(j<i):
+        i,j=j,i
+    if(l<k):
+        k,l=l,k
+    if(k<i):
+        return str(k)+','+str(l)+'|'+str(i)+','+str(j)
+    else:
+        return str(i)+','+str(j)+'|'+str(k)+','+str(l)
+def write_all_quartets(dt,out_file_path):
+    f = open(out_file_path,'w')
+    for k in dt.keys():
+            f.write(k+':'+str(dt[k])+'\n')
+    f.close() 
+def quartet_aggregate(model_dir,r,gene,numgenes,t):
+    bin=set()
+    f_bin=open(model_dir+'/R'+r+'/'+str(gene)+'/stat_bin_t_'+str(t)+'.txt','r')
+    for line in f_bin:
+        bin.add(int(line))
+    f_bin.close()
+    quartet_dict={}
+    taxon_set=set()
+    for f in range(1,numgenes+1):
+        if(f in bin):
+            f_in = open(model_dir+'/R'+r+'/'+str(f)+'/quartets_sampled_36.txt','r')
+            for line in f_in:
+                quartet=line.split(':',1)[0]
+                v=int(line.split(':',1)[1])
+                siblings=quartet.split('|',1)
+                i = siblings[0].split(',',1)[0]
+                taxon_set.add(i)
+                j = siblings[0].split(',',1)[1]
+                taxon_set.add(j)
+                k = siblings[1].split(',',1)[0]
+                taxon_set.add(k)
+                l = siblings[1].split(',',1)[1]
+                taxon_set.add(l)
+                form_q= formatted(i,j,k,l)
+                if form_q in quartet_dict:
+                    quartet_dict[form_q]+= v
+                else:
+                    quartet_dict[form_q]=v
+            f_in.close()
+            print f," Done"
+    write_all_quartets(quartet_dict, model_dir+'/R'+r+'/'+str(gene)+'/allbin_t_'+str(t)+'_quartets_sampled_36.txt')
+    print 'Total Taxa',len(taxon_set)
+if __name__ == "__main__":
+    model_dir=sys.argv[1]
+    replicate=sys.argv[2]
+    gene=int(sys.argv[3])
+    num_genes=int(sys.argv[4])
+    threshold=int(sys.argv[5])
+    quartet_aggregate(model_dir,replicate,gene,num_genes,threshold)
+    
+    
