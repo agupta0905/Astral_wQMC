4fc7f3233b62f2fa957ad711417aca5623065292
Ashu Gupta <agupta@Ashus-MacBook-Pro.local>
2015-11-13 01:33:31 -0600

    changed for non sampling

--- avian-dataset/src/adjust_quartet_weights.py
+++ avian-dataset/src/adjust_quartet_weights.py
@@ -12,10 +12,10 @@ def get_taxa(q):
     k,l = siblings[1].split(',',1)[:]
     return [i,j,k,l]
 def adjust_quartet_weight(mdir,r,g):
-    w=[5, 10, 25, 50]
+    w=[1, 5, 10, 25, 50]
     q_dict={}
     dict_list=map(lambda x: {}, w)
-    f=open(mdir+'/R'+r+'/allquartets_sampled_36_numgenes_100.txt','r')
+    f=open(mdir+'/R'+r+'/allquartets_relabeled_numgenes_100.txt','r')
     for line in f:
         q=line.split(':',1)[0]
         v=int(line.split(':',1)[1].replace('\n',''))
@@ -26,7 +26,7 @@ def adjust_quartet_weight(mdir,r,g):
         dict_list=[]
         for k in range(0,len(w)):
             dict_list.append(q_dict.copy())
-        f=open(mdir+'/R'+r+'/'+str(i)+'/quartets_sampled_36.txt','r')
+        f=open(mdir+'/R'+r+'/'+str(i)+'/quartets_relabeled.txt','r')
         for line in f:
             qlist=get_taxa(line.split(':',1)[0])
             q=formatted(qlist[0],qlist[1],qlist[2],qlist[3])
@@ -35,7 +35,7 @@ def adjust_quartet_weight(mdir,r,g):
                 dict_list[idx][q]=dict_list[idx][q]-v+weight*v
         f.close()
         for idx,weight in enumerate(w):
-            write(dict_list[idx],mdir+'/R'+r+'/'+str(i)+'/quartets_sampled_36_numgenes_100'+'_w_'+str(weight)+'.txt')  
+            write(dict_list[idx],mdir+'/R'+r+'/'+str(i)+'/quartets_relabeled_numgenes_100'+'_w_'+str(weight)+'.txt')  
         print "Gene-",i," Done" 
 if __name__ == "__main__":
     model_dir=sys.argv[1]
