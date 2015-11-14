34e67f2072f804780ecd3ba676d97e7849f00ebd
Ashu Gupta <agupta@Ashus-MacBook-Pro.local>
2015-11-13 18:39:19 -0600

    Add weighting

--- avian-dataset/src/adjust_quartet_weights_binning.py
+++ avian-dataset/src/adjust_quartet_weights_binning.py
@@ -16,7 +16,7 @@ def adjust_quartet_weight(mdir,r,g,t):
     dict_list=map(lambda x: {}, w)
     for i in range(1,g+1):
         q_dict={}
-        f=open(mdir+'/R'+r+'/'+str(i)+'/allbin_t_'+str(t)+'_quartets_sampled_36.txt','r')
+        f=open(mdir+'/R'+r+'/'+str(i)+'/allorigbin_t_'+str(t)+'_quartets_relabeled.txt','r')
         for line in f:
             q=line.split(':',1)[0]
             v=int(line.split(':',1)[1].replace('\n',''))
@@ -26,7 +26,7 @@ def adjust_quartet_weight(mdir,r,g,t):
         dict_list=[]
         for k in range(0,len(w)):
             dict_list.append(q_dict.copy())
-        f=open(mdir+'/R'+r+'/'+str(i)+'/quartets_sampled_36.txt','r')
+        f=open(mdir+'/R'+r+'/'+str(i)+'/quartets_relabeled.txt','r')
         for line in f:
             qlist=get_taxa(line.split(':',1)[0])
             q=formatted(qlist[0],qlist[1],qlist[2],qlist[3])
@@ -35,7 +35,7 @@ def adjust_quartet_weight(mdir,r,g,t):
                 dict_list[idx][q]=dict_list[idx][q]-v+weight*v
         f.close()
         for idx,weight in enumerate(w):
-            write(dict_list[idx],mdir+'/R'+r+'/'+str(i)+'/allbin_t_'+str(t) +'_quartets_sampled_36'+'_w_'+str(weight)+'.txt')  
+            write(dict_list[idx],mdir+'/R'+r+'/'+str(i)+'/allorigbin_t_'+str(t) +'_quartets_relabeled_'+'_w_'+str(weight)+'.txt')  
         print "Gene-",i," Done" 
 if __name__ == "__main__":
     model_dir=sys.argv[1]
